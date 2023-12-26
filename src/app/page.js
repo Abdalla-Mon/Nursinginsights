ea545576fdc75be80b5c6b95afbbd90e6bee3e37
AbdallaMon <abdotlos60@gmail.com>
2023-12-26 22:24:41 +0200

    added shared component

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,4 @@
-"use client";
-import { selectAuthState, setAuthState } from "../lib/redux/slices/testSlice";
-import { useDispatch, useSelector } from "react-redux";
 function Home() {
-  const authState = useSelector(selectAuthState);
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <div>{authState.authState ? "Logged in" : "Not Logged In"}</div>
-      <button
-        onClick={() =>
-          authState.authState
-            ? dispatch(setAuthState(false))
-            : dispatch(setAuthState(true))
-        }
-      >
-        {authState.authState ? "Logout" : "LogIn"}
-        <div>{authState.num}</div>
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
 export default Home;
