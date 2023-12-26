e153216328f37b66ec843b077725a20578a4afa7
AbdallaMon <abdotlos60@gmail.com>
2023-12-26 22:06:45 +0200

    nothin important

--- src/lib/auth/facebook/facebook_auth.js
+++ src/lib/auth/facebook/facebook_auth.js
@@ -1,42 +1,36 @@
-// // make a login
-// window.fbAsyncInit = function () {
-//   FB.init({
-//     appId: "{your-app-id}",
-//     cookie: true,
-//     xfbml: true,
-//     version: "{api-version}",
-//   });
+// make a login
+export function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+}
 
-//   FB.AppEvents.logPageView();
-// };
+export function facebookLogin() {
+  FB.login(function (response) {
+    console.log(response);
+  });
+}
+export function facebookInit() {
+  window.fbAsyncInit = function () {
+    FB.init({
+      appId: "{your-app-id}",
+      cookie: true,
+      xfbml: true,
+      version: "{api-version}",
+    });
 
-// (function (d, s, id) {
-//   var js,
-//     fjs = d.getElementsByTagName(s)[0];
-//   if (d.getElementById(id)) {
-//     return;
-//   }
-//   js = d.createElement(s);
-//   js.id = id;
-//   js.src = "https://connect.facebook.net/en_US/sdk.js";
-//   fjs.parentNode.insertBefore(js, fjs);
-// })(document, "script", "facebook-jssdk");
+    FB.AppEvents.logPageView();
+  };
 
-// //    check if login
-// FB.getLoginStatus(function (response) {
-//   statusChangeCallback(response);
-// });
-
-// import { auth } from "../../firebase_config/firebase_conig";
-// import { FacebookAuthProvider, signInWithPopup } from "firebase/auth";
-// export const handleFacebookSignIn = async () => {
-//   try {
-//     const provider = new FacebookAuthProvider();
-//     const result = await signInWithPopup(auth, provider);
-//     console.log(result);
-//     const user = result.user;
-//     // User is signed in
-//   } catch (error) {
-//     // Handle sign-in errors
-//   }
-// };
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+}
