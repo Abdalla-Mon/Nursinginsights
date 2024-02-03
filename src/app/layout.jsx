e9eafc98486c8e69427aa8fee3acf9074f2f7cf7
Abdalla Abdelsabour <abdotlos60@gmail.com>
2024-02-03 18:20:33 +0200

    modified some layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,7 +29,7 @@ export const cubid = localFont({
   ],
 });
 export const metadata = {
-  title: "Nursing Insights - Empowering Nursing Excellence",
+  title: "Nursing Insights",
   description:
     "Unlock the path to nursing excellence with NursingInsights – your comprehensive online resource for students and professionals in the field of nursing. Explore enriching lectures, insightful videos, and interactive quizzes designed to elevate your academic journey. Delve into department-specific information, guiding you to excel in every facet of your college experience. Join us in fostering a community committed to the art and science of nursing. Empower your knowledge, embrace your potential, and thrive in the world of healthcare at NursingInsights.",
   keyWords:
--- src/app/nursingcareplans/layout.js
+++ src/app/nursingcareplans/layout.js
@@ -1,17 +1,11 @@
 export const metadata = {
-    title: "Nursing Insights Care Plans - Empowering Nursing Excellence",
-    description:
-        "Explore comprehensive care plans at NursingInsights – your online resource for students and professionals in the field of nursing. Our care plans are designed to elevate your academic journey and guide you to excel in every facet of your healthcare career. Join us in fostering a community committed to the art and science of nursing. Empower your knowledge, embrace your potential, and thrive in the world of healthcare with our care plans at NursingInsights.",
-    keyWords:
-        "Nursing, nursing education, healthcare, student resources, nursing excellence, nursing care plans",
+  title: "Nursing Care Plans - Nursing insight Empowering Nursing Excellence",
+  description:
+    "Explore comprehensive care plans at NursingInsights – your online resource for students and professionals in the field of nursing. Our care plans are designed to elevate your academic journey and guide you to excel in every facet of your healthcare career. Join us in fostering a community committed to the art and science of nursing. Empower your knowledge, embrace your potential, and thrive in the world of healthcare with our care plans at NursingInsights.",
+  keyWords:
+    "Nursing, nursing education, healthcare, student resources, nursing excellence, nursing care plans",
 };
 
 export default function CoursesLayout({ children }) {
-    return (
-        <>
-            {children}
-        </>
-
-
-    );
+  return <>{children}</>;
 }
