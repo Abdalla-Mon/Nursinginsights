b6fb0d57442b9d44889d7fd0bcadf0dbbff0f660
Abdalla Abdelsabour <abdotlos60@gmail.com>
2024-02-18 15:09:27 +0200

    handle build error

--- src/app/(private)/dashboard/courses/page.jsx
+++ src/app/(private)/dashboard/courses/page.jsx
@@ -1,18 +1,8 @@
-"use client";
-import CreateModal from "@/app/(private)/dashboard/courses/components/CreateFormModal/CreateModal";
-import CoursesData from "@/app/(private)/dashboard/courses/CoursesData";
-import { useState } from "react";
-import DashMenu from "@/app/(private)/dashboard/DashboardMenu/DashMenu";
+import DashboardCourses from "@/app/(private)/dashboard/courses/Courses";
+
 
 export default function DashboardPage() {
-  const [reValidate, setReValidate] = useState(false);
   return (
-    <div className={"flex flex-row gap-8 justify-center"}>
-      <DashMenu />
-      <div className={"w-full tab:w-[70%]"}>
-        <CreateModal setReValidate={setReValidate} />
-        <CoursesData reValidate={reValidate} />
-      </div>
-    </div>
+          <DashboardCourses />
   );
 }
