48ec87e6e7b57f87335f21feae40faf92fdf2fb7
Abdalla Abdelsabour <abdotlos60@gmail.com>
2024-02-18 15:15:13 +0200

    modified the url

--- src/lib/fetch_data/getData.js
+++ src/lib/fetch_data/getData.js
@@ -2,7 +2,7 @@ export default async function getData(path) {
   const productionUrl = "https://nursinginsights.vercel.app";
   const url = "http://localhost:3000";
 
-  const res = await fetch(`${url}${path}`);
+  const res = await fetch(`${productionUrl}${path}`);
   const data = await res.json();
   return data;
 }
--- src/lib/fetch_data/url.js
+++
@@ -1,4 +0,0 @@
-export const url = {
-  localUrl: "http:localhost:3000/api",
-  productionUrl: "https://nursinginsights.vercel.app",
-};
